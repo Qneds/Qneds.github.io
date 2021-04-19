38eed885b1e70af13ecd4c487dd0b4ef98677710
Qneds <pin20424@gmail.com>
2021-04-19 23:41:55 +0200

    #3

--- lab3/code.js
+++ lab3/code.js
@@ -61,7 +61,7 @@ const StudentBox = (props) => {
 
 const NumberOfFoundedStudnets = (props) => {
     return (
-        <div>
+        <div className="found_el">
             Znaleziono {props.numberOfStudent} studentów
         </div>
     )
@@ -384,3 +384,114 @@ ReactDOM.render(
     <Main/>,
     document.getElementById('root')
 );
+
+
+class Autocomplete extends React.Component {
+
+    state = {
+      activeOption: 0,
+      filteredOptions: [],
+      showOptions: false,
+      userInput: ''
+    };
+  
+    onChange = (event) => {
+  
+      const { options } = this.props;
+      const userInput = e.currentTarget.value;
+  
+      const filteredOptions = options.filter(
+        (optionName) =>
+          optionName.toLowerCase().indexOf(userInput.toLowerCase()) > -1
+      );
+  
+      this.setState({
+        activeOption: 0,
+        filteredOptions,
+        showOptions: true,
+        userInput: e.currentTarget.value
+      });
+    };
+  
+    onClick = (e) => {
+      this.setState({
+        activeOption: 0,
+        filteredOptions: [],
+        showOptions: false,
+        userInput: e.currentTarget.innerText
+      });
+    };
+    onKeyDown = (e) => {
+      const { activeOption, filteredOptions } = this.state;
+  
+      if (e.keyCode === 13) {
+        this.setState({
+          activeOption: 0,
+          showOptions: false,
+          userInput: filteredOptions[activeOption]
+        });
+      } else if (e.keyCode === 38) {
+        if (activeOption === 0) {
+          return;
+        }
+        this.setState({ activeOption: activeOption - 1 });
+      } else if (e.keyCode === 40) {
+        if (activeOption === filteredOptions.length - 1) {
+          console.log(activeOption);
+          return;
+        }
+        this.setState({ activeOption: activeOption + 1 });
+      }
+    };
+  
+    render() {
+      const {
+        onChange,
+        onClick,
+        onKeyDown,
+  
+        state: { activeOption, filteredOptions, showOptions, userInput }
+      } = this;
+      let optionList;
+      if (showOptions && userInput) {
+        if (filteredOptions.length) {
+          optionList = (
+            <ul className="options">
+              {filteredOptions.map((optionName, index) => {
+                let className;
+                if (index === activeOption) {
+                  className = 'option-active';
+                }
+                return (
+                  <li className={className} key={optionName} onClick={onClick}>
+                    {optionName}
+                  </li>
+                );
+              })}
+            </ul>
+          );
+        } else {
+          optionList = (
+            <div className="no-options">
+              <em>No Option!</em>
+            </div>
+          );
+        }
+      }
+      return (
+        <>
+          <div className="search">
+            <input
+              type="text"
+              className="search-box"
+              onChange={onChange}
+              onKeyDown={onKeyDown}
+              value={userInput}
+            />
+            <input type="submit" value="" className="search-btn" />
+          </div>
+          {optionList}
+        </>
+      );
+    }
+  }
